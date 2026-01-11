b842ca110c04ff38538485a5065d17196fee87c3
iiGray <iiiigray19@gmail.com>
2026-01-11 15:03:40 +0000

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,6 +16,8 @@ version = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
+master_doc = 'index'
+
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
