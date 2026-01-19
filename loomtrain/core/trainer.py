5b1c69c75e0351010622a1def322f4d7816961a5
iiGray <iiiigray19@gmail.com>
2026-01-19 15:32:59 +0000

    reconstruct trainer: add `fit`

--- loomtrain/core/trainer.py
+++ loomtrain/core/trainer.py
@@ -1,68 +1,104 @@
 from loomtrain.core.state import CheckpointConfig
 from loomtrain.core.strategy import TrainStrategy, DataStrategy
-from loomtrain.core.module import LoomModule
-from loomtrain.core.datamodule import LoomDataModule
-from loomtrain.core.visualization import VisualizationModule
+from loomtrain.core.module import Module
+from loomtrain.core.datamodule import DataModule
+from loomtrain.core.visualization import NoneVisualization, VisualizationModule
+from loomtrain.core.arguments import args
+
+from accelerate import accelerator
 
 
-class LoomTrainer:
-    ''''''
-    def __init__(
-            self, *,
-            train_strategy: "TrainStrategy" = None,
-            data_strategy: "DataStrategy" = None,
-            accelerator = None,
-            devices = None,
-
-    ):
-        self.train_strategy = train_strategy
-        self.data_stretegy = data_strategy
-        self.accelerator = accelerator # TODO
-        self.devices = devices # TODO
-        self.train_strategy.setup_distributed()
-
-    def fit(self, 
-            module: "LoomModule", 
-            datamodule: "LoomDataModule",
-            vismodule: "VisualizationModule",
-            checkpoint_config: "CheckpointConfig"):
-        
-        datamodule.connect_module(module)
-        datamodule.connect_strategy(self.data_stretegy)
-        module.connect_datamodule(datamodule)
-        module.connect_strategy(self.train_strategy)
-
-
-        if checkpoint_config.do_resume:
-            module._load_ckpt(checkpoint_config)
-            datamodule._load_ckpt(checkpoint_config, inplace = True)
-            vismodule._load_ckpt(checkpoint_config, inplace = True)
-
-        module.train()
-        datamodule.train()
+def fit(module: "Module",
+        datamodule: "DataModule",
+        train_strategy: "TrainStrategy" = None,
+        data_strategy: "DataStrategy" = None,
+        vismodule: "VisualizationModule" = None,
+        checkpoint_config: "CheckpointConfig" = None,
+        enable_distributed: bool = True):
     
-        while not datamodule.exhausted:
-            batches = datamodule._update()
-            logs_dict = dict()
-            state_dict = module._update(batches)
+    if train_strategy is None:
+        train_strategy = TrainStrategy(
+            global_batch_size = args().global_batch_size,
+            micro_batch_size = args().micro_batch_size,
+            val_batch_size = args().val_batch_size,
+            cp_size = args().cp_size,
+            cp_type = args().cp_type,
+            cp_args = args().cp_args,
+            val_interval = args().val_interval
+        )
+    if data_strategy is None:
+        data_strategy = DataStrategy(
+            packing_length = args().packing_length
+        )
 
-            for k, v in state_dict.items():
-                logs_dict[f"train/{k}"] = v
+    if checkpoint_config is None:
+        checkpoint_config = CheckpointConfig(
+            save_dir = args().save_dir,
+            ckpt_interval = args().ckpt_interval,
+            weight_interval = args().weight_interval,
+            visulization_interval = args().visulization_interval,
+            max_ckpts = args().max_ckpts,
+            max_ckpts_GB = args().max_ckpts_GB
+        )
+    if (vismodule is None) and args().logtype:
+        vismodule = VisualizationModule(
+            logtype = args().logtype,
+            wandb_api = args().wandb_api,
+            wandb_entity = args().wandb_entity,
+            wandb_project = args().wandb_project,
+            wandb_group = args().wandb_group,
+            wandb_name = args().wandb_name
+        )
+    if vismodule is None: vismodule = NoneVisualization()
+        
+    if enable_distributed:
+        train_strategy.setup_distributed()
 
-            state_dict = module._validate(datamodule)
+    module._initialize()
+    datamodule._initialize()
 
-            for k, v in state_dict.items():
-                logs_dict[f"val/{k}"] = v
-            
-            vismodule._update(state_dict)
+    if data_strategy is not None:
+        datamodule._connect_strategy(data_strategy)
+    datamodule._connect_module(module)
+    if train_strategy is not None:
+        module._connect_strategy(train_strategy)
+        train_strategy._connect_datamodule(datamodule)
+    module._connect_datamodule(datamodule)
+    
+    module.config_module()
 
-            datamodule._save_ckpt(checkpoint_config, inplace = True)
-            module._save_ckpt(checkpoint_config)
-            vismodule._save_ckpt(checkpoint_config, inplace = True)
-
-            module._save_module(checkpoint_config) # save module weights for inference
-
-        vismodule.release()
-
+    module.zero_grad()
 
 
+
+    if checkpoint_config.do_resume:
+        module._load_ckpt(checkpoint_config)
+        datamodule._load_ckpt(checkpoint_config, inplace = True)
+        vismodule._load_ckpt(checkpoint_config, inplace = True)
+
+
+    module.train()
+    datamodule.train()
+
+    while not datamodule.exhausted:
+        batches = datamodule._update_()
+        logs_dict = dict()
+        state_dict = module._update_(batches)
+
+        for k, v in state_dict.items():
+            logs_dict[f"train/{k}"] = v
+
+        state_dict = module._validate(datamodule)
+
+        for k, v in state_dict.items():
+            logs_dict[f"val/{k}"] = v
+        
+        vismodule._update_(state_dict)
+
+        datamodule._save_ckpt(checkpoint_config, inplace = True)
+        module._save_ckpt(checkpoint_config)
+        vismodule._save_ckpt(checkpoint_config, inplace = True)
+
+        module._save_module(checkpoint_config) # save module weights for inference
+
+    vismodule.release()
