45d3ef34d96794b2e016d180fc23dd6863d3156d
iiGray <iiiigray19@gmail.com>
2026-01-19 15:31:15 +0000

    implement `SortPackingStrategy`

--- loomtrain/core/strategies/data/sort_packing.py
+++ loomtrain/core/strategies/data/sort_packing.py
@@ -1,43 +1,37 @@
 from typing import List
 import torch.utils.data as tud
 
-from loomtrain.core.distributed_sampler import (
+from loomtrain.core.data.sampler import (
     DistributedSampler, DistributedBucketSampler
 )
 
-from loomtrain.core.data.dataset.base import CollateDataset
+from loomtrain.core.data.dataset.base import Dataset
 
 from loomtrain.core.parallel import parallel_state as parallel
 from loomtrain.core.strategy import DataConfig, DataStrategy
-from loomtrain.core.data.dataloader.iter import LoomDataIter
+from loomtrain.core.data.dataloader.iter import DataIter
 
 
 class SortPackingStrategy(DataStrategy):
-    def __init__(self,
-                 parallel_config: "parallel.ParallelConfig",
-                 data_config: "DataConfig",
-                 full_determinism: "bool" = False,
-                 seed:int = 42
-                 ):
-        super().__init__(parallel_config = parallel_config, 
-                         data_config = data_config,
-                         full_determinism = full_determinism, 
-                         seed = seed)
-
-    def setup_data_iter(self, dataset: "CollateDataset") -> "LoomDataIter":
+        
+    def setup_data_iter(self, dataset: "Dataset") -> "DataIter":
 
         Sampler = DistributedSampler
         sampler_type = 'sampler'
-
         dataloader_kwargs = dict(
             batch_size = self.data_config.micro_batch_size,
             num_epochs = self.data_config.num_epochs,
-            pin_memory = self.data_config.pin_memory
+            collate_fn = self.datamodule.collate_fn,
+            pin_memory = self.data_config.pin_memory,
+            length_key = "input_ids_lens"
         )
         if self.data_config.packing_length:
             Sampler = DistributedBucketSampler
             sampler_type = 'batch_sampler'
             dataloader_kwargs.pop('batch_size')
+        else:
+            dataloader_kwargs.pop("length_key")
+
         
         dataloader_kwargs[sampler_type] = Sampler(
             dataset,
@@ -50,13 +44,20 @@ class SortPackingStrategy(DataStrategy):
             drop_exceed = self.data_config.drop_exceed
         )
 
-        return LoomDataIter(
+        return DataIter(
             dataset, 
             ** dataloader_kwargs
         )
 
-    def loomDataModule_save_ckpt(self, save_dir, tag):
+    def setup_train_data_iter(self):
+        return self.setup_data_iter(self.datamodule.get_train_dataset())
+
+    def setup_val_data_iter(self):
+        return self.setup_data_iter(self.datamodule.get_val_dataset())
+    
+
+    def save_ckpt(self, save_dir, tag):
         raise NotImplementedError
 
-    def loomDataModule_load_ckpt(self, saved_dir, tag):
+    def load_ckpt(self, saved_dir, tag):
         raise NotImplementedError
