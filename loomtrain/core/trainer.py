5acb249b778641bce0cb090fe5d599485b9bb4d7
iiGray <iiiigray19@gmail.com>
2026-01-24 13:08:10 +0000

    add terminal visualization

--- loomtrain/core/trainer.py
+++ loomtrain/core/trainer.py
@@ -1,4 +1,8 @@
 from tqdm import tqdm
+from rich.live import Live
+from rich.table import Table
+from rich.progress import Progress, SpinnerColumn, BarColumn, TextColumn
+from rich.console import Group
 from loomtrain.core.state import CheckpointConfig
 from loomtrain.core.strategy import TrainStrategy, DataStrategy
 from loomtrain.core.module import Module
@@ -6,6 +10,14 @@ from loomtrain.core.datamodule import DataModule
 from loomtrain.core.visualization import NoneVisualization, VisualizationModule
 from loomtrain.core.parallel import parallel_state as parallel
 from loomtrain.core.arguments import args
+
+def _generate_table(log_dicts):
+    table = Table(show_header=False, padding=(0, 1))
+    table.add_column("Key", justify="left")
+    table.add_column("Value", justify="left", no_wrap=True)
+    for k, v in log_dicts.items():
+        table.add_row(f"{k}:", str(v))
+    return table
 
 
 def fit(module: "Module",
@@ -70,7 +82,6 @@ def fit(module: "Module",
     module.zero_grad()
 
 
-
     if checkpoint_config.do_resume:
         module._load_ckpt(checkpoint_config)
         datamodule._load_ckpt(checkpoint_config, inplace = True)
@@ -80,34 +91,73 @@ def fit(module: "Module",
     module.train()
     datamodule.train()
 
-    progress_bar = tqdm(range(0, datamodule.total_train_steps), 
-                        desc = f"Training epoch: {datamodule.training_epoch + 1}/{args().num_epochs}", 
-                        initial = datamodule.consumed_steps,
-                        disable = parallel.get_rank() != 0)
+    if args().terminal_logtype == "tqdm":
+        progress_bar = tqdm(range(0, datamodule.total_train_steps), 
+                            desc = f"Training epoch: {datamodule.training_epoch + 1}/{args().num_epochs}", 
+                            initial = datamodule.consumed_steps,
+                            position = 0, dynamic_ncols = True, 
+                            disable = parallel.get_rank() != 0)
+    elif parallel.get_rank() == 0:
+        progress = Progress(
+            SpinnerColumn(),
+            TextColumn("[progress.description]{task.description}"),
+            BarColumn(
+                style = "yellow",
+                complete_style = "cyan",
+                finished_style = "green"
+            ),
+            TextColumn("{task.completed:.0f}/{task.total:.0f}"),
+            disable = parallel.get_rank() != 0
+        )
+        training_task = progress.add_task("Total Training Steps:", 
+                                          start = True,
+                                          completed = datamodule.consumed_steps,
+                                          total = datamodule.total_train_steps)
+        live = Live(Group(progress), refresh_per_second=10)
+        live.start()
 
-    while not datamodule.exhausted:
-        batches = datamodule._update_()
-        logs_dict = dict()
-        state_dict = module._update_(batches)
+    ################################
+    ############ Training Loop ############
+    try:
+        while not datamodule.exhausted:
+            batches = datamodule._update_()
+            logs_dict = dict()
+            state_dict = module._update_(batches)
 
-        for k, v in state_dict.items():
-            logs_dict[f"train/{k}"] = v
+            for k, v in state_dict.items():
+                logs_dict[f"train/{k}"] = v
 
-        progress_bar.set_description(f"Training epoch: {datamodule.training_epoch + 1}/{args().num_epochs}")
-        progress_bar.set_postfix(logs_dict)
-        progress_bar.update(1)
-        state_dict = module._validate(datamodule)
+            if args().terminal_logtype == "tqdm":
+                progress_bar.set_description(f"Training epoch: {datamodule.training_epoch + 1}/{args().num_epochs}")
+                progress_bar.set_postfix(logs_dict)
+                progress_bar.update(1)
+            
+            state_dict = module._validate(datamodule)
 
-        for k, v in state_dict.items():
-            logs_dict[f"val/{k}"] = v
-        
-        vismodule._update_(state_dict)
+            if args().terminal_logtype == "rich" and parallel.get_rank() == 0:
+                progress.update(training_task, advance = 1)
+                live.update(
+                    Group(
+                        progress,
+                        _generate_table({"Training Epoch:" : f"{datamodule.training_epoch + 1}/{args().num_epochs}",
+                                         "Consumed Samples:" : str(datamodule.train_data_iter.consumed_samples), 
+                                         ** logs_dict})
+                    )
+                )
 
-        datamodule._save_ckpt(checkpoint_config, inplace = False)
-        module._save_ckpt(checkpoint_config, inplace = False, update_tag = True)
-        vismodule._save_ckpt(checkpoint_config, inplace = True)
+            for k, v in state_dict.items():
+                logs_dict[f"val/{k}"] = v
+            
+            vismodule._update_(state_dict)
 
-        module._save_module(checkpoint_config) # save module weights for inference
+            datamodule._save_ckpt(checkpoint_config, inplace = False)
+            module._save_ckpt(checkpoint_config, inplace = False, update_tag = True)
+            vismodule._save_ckpt(checkpoint_config, inplace = True)
 
-    progress_bar.close()            
-    vismodule.release()
+            module._save_module(checkpoint_config) # save module weights for inference
+    finally:
+        if args().terminal_logtype == "tqdm":
+            progress_bar.close()
+        elif parallel.get_rank() == 0:
+            live.stop()            
+        vismodule.release()
